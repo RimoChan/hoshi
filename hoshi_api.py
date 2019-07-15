1c06e402393e4cfa05a79b16c6c5f12c68ef7019
Rimo <RimoChan@stu.xmu.edu.cn>
2019-07-15 17:18:50 +0800

    添加api

--- hoshi_api.py
+++ hoshi_api.py
@@ -5,6 +5,7 @@ import cv2
 import hoshi本体.hoshi
 import hoshi本体.表格识别
 import hoshi本体.输出doc
+import hoshi本体.旋转矫正
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -13,6 +14,12 @@ logging.basicConfig(level=logging.DEBUG)
 def pdf_to_word(pdf_file_name, word_file_name, brightness_threshold=166, dpi=600, thread_count=3):
     页组 = hoshi本体.hoshi.星(pdf_file_name, brightness_threshold).龙(dpi=dpi, 线程数=thread_count)
     hoshi本体.输出doc.输出(word_file_name, 页组)
+
+
+def image_to_word(image_file_name, word_file_name, brightness_threshold=166):
+    img = cv2.imread(image_file_name)
+    页 = hoshi本体.hoshi.星(None, brightness_threshold).单图片提取(img)
+    hoshi本体.输出doc.输出(word_file_name, [页])
 
 
 def extract_form_image(image_file_name):
@@ -26,6 +33,23 @@ def extract_form_image(image_file_name):
     return 表格位置组, 图片组
 
 
-if __name__=='__main__':
-    pdf_to_word('.\hoshi本体\data\SYT 6662.5-2014.pdf', 'final.docx', thread_count=2)
+def extract_form(image_file_name):
+    img = cv2.imread(image_file_name)
+    img_noko, 表格组 = hoshi本体.表格识别.分割表格(img, 'nya')
+    return 表格组
+
+
+def image_rotation_correction(image_file_name):
+    img = cv2.imread(image_file_name)
+    d = hoshi本体.旋转矫正.自动旋转矫正(img)
+    return d
+
+
+if __name__ == '__main__':
+    # pdf_to_word('.\hoshi本体\data\SYT 6662.5-2014.pdf', 'final.docx', thread_count=2)
     # pdf_to_word('.\hoshi本体\data\librian.pdf', 'final.docx', 255, thread_count=2)
+    image_to_word('0.png', 'final.docx')
+    
+    # print(extract_form('./2.png'))
+    # cv2.imwrite('r0.png', image_rotation_correction('./r-3.png'))
+    
