125d187a6964bfe9c520ebf8484cd4720b7173b6
Rimo <RimoChan@stu.xmu.edu.cn>
2019-07-16 15:11:10 +0800

    我又发明了有的东西……

--- hoshi本体/util.py
+++ hoshi本体/util.py
@@ -9,12 +9,11 @@ def 屑检测(img, 面积阈值):
 
 def 局部二值化(img, d):
     d = max(d, 3)
-    img_bin = img.copy()
-    t = cv2.blur(img_bin, (d, d), 0).astype(np.int)
-    img_bin[np.where(img_bin > t - 5)] = 255
-    img_bin[np.where(img_bin <= t - 5)] = 0
-    # img_bin[np.where(img_bin - t + 5 < 128)] = 255   # 为什么这个写法不对？？？
-    # img_bin[np.where(img_bin - t + 5 >= 128)] = 0
+    # img_bin = img.copy()
+    # t = cv2.blur(img_bin, (d, d), 0).astype(np.int)
+    # img_bin[np.where(img_bin > t - 5)] = 255
+    # img_bin[np.where(img_bin <= t - 5)] = 0
+    img_bin = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, d, 6)
     return img_bin
 
 
